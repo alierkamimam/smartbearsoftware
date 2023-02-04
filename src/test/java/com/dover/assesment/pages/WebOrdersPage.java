fc8ccabc9f4e7b8fac84d178386ea369d61078c7
ALİ ERKAM İMAM <imam.ali.erkam@gmail.com>
2023-02-04 16:58:48 +0300

    added

--- src/test/java/com/dover/assesment/pages/WebOrdersPage.java
+++ src/test/java/com/dover/assesment/pages/WebOrdersPage.java
@@ -7,44 +7,60 @@ import org.openqa.selenium.support.FindBy;
 
 public class WebOrdersPage extends BasePage {
 
-    @FindBy(id = "ctl00_MainContent_username")
-    private WebElement userNameInputBoxElement;
+    @FindBy(xpath = "//div[@class='login_info']")
+    private WebElement displayedUserName;
 
-    @FindBy(id = "ctl00_MainContent_password")
-    private WebElement passwordInputBoxElement;
+    @FindBy(xpath = "//h1")
+    private WebElement levelOneHeader;
 
-    @FindBy(id = "ctl00_MainContent_login_button")
-    private WebElement loginButton;
+    @FindBy(id = "ctl00_logout")
+    private WebElement logoutButton;
 
-    @FindBy(id = "ctl00_MainContent_status")
-    private WebElement errorMessage;
+    @FindBy(xpath = "//a[@href='Default.aspx']")
+    private WebElement viewAllOrdersButton;
 
+    @FindBy(xpath = "//a[@href='Products.aspx']")
+    private WebElement viewAllOProductsButton;
 
-    public void login() {
-        String usernameValue = ConfigReader.getProperty("username");
-        String passwordValue = ConfigReader.getProperty("password");
-        userNameInputBoxElement.sendKeys(usernameValue);
-        passwordInputBoxElement.sendKeys(passwordValue, Keys.ENTER);
-    }
+    @FindBy(xpath = "//a[@href='Process.aspx']")
+    private WebElement orderButton;
 
-
-    public void enterUserNameValue(String usernameValue) {
-        userNameInputBoxElement.sendKeys(usernameValue);
-    }
-
-    public void enterPasswordValue(String passwordValue) {
-        passwordInputBoxElement.sendKeys(passwordValue);
-    }
-
-    public void clickLoginButton() {
-        loginButton.click();
-    }
-
-    public String getErrorMessage() {
-        return errorMessage.getText();
-    }
 
     public String getCurrentUrl() {
         return driver.getCurrentUrl();
+    }
+
+    public String getDisplayedUserName() {
+        return displayedUserName.getText();
+    }
+
+    public String getHeaderAsString(Integer levelOfHeader) {
+        String result = null;
+        switch (levelOfHeader) {
+            case 1:
+                result = levelOneHeader.getText();
+                break;
+        }
+        return result;
+    }
+
+    public void clickLogoutButton() {
+        logoutButton.click();
+    }
+
+    public boolean isVerticalMenuBarItemDisplayed(String verticalMenuBarItem) {
+        boolean result = false;
+        switch (verticalMenuBarItem) {
+            case "View all orders":
+                result = viewAllOrdersButton.isDisplayed();
+                break;
+            case "View all products":
+                result = viewAllOProductsButton.isDisplayed();
+                break;
+            case "Order":
+                result = orderButton.isDisplayed();
+                break;
+        }
+        return result;
     }
 }
